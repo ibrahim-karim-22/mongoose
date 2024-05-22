e17f00d7446c9ccbe5c0a0d02a5889331698b7d8
karim <hema_thechamp@yahoo.com>
2024-05-21 21:10:28 -0400

    Mongoose Part 3

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const url = "mongodb://localhost:27017/nucampsite";
 const connect = mongoose.connect(url, {});
 
 connect.then(() => {
-
   console.log("Connected correctly to server");
 
   Campsite.create({
@@ -13,12 +12,32 @@ connect.then(() => {
     description: "test",
   })
 
+    .then((campsite) => {
+      console.log(campsite);
+
+      return Campsite.findByIdAndUpdate(
+        campsite._id,
+        {
+          $set: { description: "Updated Test Document" },
+        },
+        {
+          new: true,
+        }
+      );
+    })
+
+    .then((campsite) => {
+      console.log(campsite);
+
+      campsite.comments.push({
+        rating: 5,
+        text: "What a magnificent view!",
+        author: "Tinus Lorvaldes",
+      });
+      return campsite.save();
+    })
     .then(campsite => {
       console.log(campsite);
-      return Campsite.find();
-    })
-    .then(campsites => {
-      console.log(campsites);
       return Campsite.deleteMany();
     })
     .then(() => {
